6857c66b17c2461238f330624f89169a1d9ad2a5
Otávio Augusto Souza da Fonseca <104402821+OtavioFonsec@users.noreply.github.com>
2025-05-25 10:17:04 -0300

    Update api.js

--- BibliocantoFront/BibliocantoTCC-MainFront/src/services/api.js
+++ BibliocantoFront/BibliocantoTCC-MainFront/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://191.235.228.35/'
+    baseURL: 'https://bibliocantobackend-ejdcdghpamcydde8.brazilsouth-01.azurewebsites.net'
 });
 
 api.validarToken = async () => {
